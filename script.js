1cab512e4963c77adb07df24fe79ab3f4bed5c72
praanjal7 <praanjalpagar@gmail.com>
2025-06-23 15:19:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -12,8 +12,5 @@ function isSameType(value1, value2) {
 let value1 = prompt("Enter Start of the Range.");
 let value2 = prompt("Enter End Of the Range.");
 
-// Try to parse numeric input to actual numbers if possible
-let parsed1 = isNaN(Number(value1)) ? value1 : Number(value1);
-let parsed2 = isNaN(Number(value2)) ? value2 : Number(value2);
 
-alert(isSameType(parsed1, parsed2));
+alert(isSameType(value1, value2));
